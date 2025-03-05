962a34b4bb40ef5c2f86aaab07d5a865a8044254
ericnrolll <fdsindayen@up.edu.ph>
2025-03-05 18:26:49 +0800

    corrected typo errors in test.js

--- test.js
+++ test.js
@@ -2,7 +2,7 @@ import needle from "needle";
 
 needle.post(
     'http://localhost:3000/add-book',
-    { bookName: "Harry Potter and the Philosopher's Stone", isbn: "966-0-7475-3269-9", author: "J.K Rowling", yearPublished: "1997" },
+    { bookName: "Harry Potter and the Philosopher's Stone", isbn: "978-0-7475-3269-9", author: "J.K Rowling", yearPublished: "1997" },
     (err, res) => {
         console.log(res.body);
     }
